cb4d71c3a6ae4ee08aacf63a42a76058c7e170cf
Julia Vu <juliadvu@gmail.com>
2020-06-23 23:19:03 -0500

    working on method to generate grid

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -3,9 +3,9 @@ import React, {useState} from 'react';
 //set up initial state with useState hook
 const [generations, setGenerations] = useState(0)
 const [speed, setSpeed] = useState(100)
-const [rows, setRow] = useState(40)
-const [columns, setColumns] = useState(60)
-const [grid, setGrid] = useState(Array(rows).fill().map(()=>Array(columns).fill(false)))
+const [numRows, setNumRow] = useState(40)
+const [numColumns, setNumColumns] = useState(60)
+const [grid, setGrid] = useState(Array(numRows).fill().map(()=>Array(numColumns).fill(false)))
 
 //location of neighboring cells
 const operations =[
@@ -19,9 +19,14 @@ const operations =[
     [-1, 0],
 ]
 
-const select =(rows, columns)=>{
+const select =(row, column)=>{
     let gridCopy = JSON.parse(JSON.stringify(grid)) //creates copy of grid. Double Buffering
-    gridCopy[rows][columns] =!gridCopy[rows][columns]
+    gridCopy[row][column] =!gridCopy[row][column] //default grid copy no longer valid
+
+    // gridCopy[row][columns] =gridCopy[row][column] > 0? 0: 1;
+     //if number of row and columns greater than 0, set default to 0. dead. else set to alive 
+
+     //change new setGrid state to new gridCopy from box selected
     setGrid(gridCopy)
 }
 
