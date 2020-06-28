b0b7adfe4c18fdc5523cc4ec512f3f42c83ec5e4
Julia Vu <juliadvu@gmail.com>
2020-06-27 22:39:47 -0500

    downloaded npm installed immer library

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import produce from "immer";
 
 //location of neighboring cells
@@ -25,17 +25,66 @@ const generateEmptyGrid =(numRows, numCols)=>{
 
 function Grid(){
     //initial states
-    const [running, setRunning] = useState(false);
     const [speed, setSpeed] = useState(100);
+    const [generations, setGenerations] = useState(0);
     
-    const [gridValues ={numRows, numCols}, setGridValues] = useState({
+    const [{numRows, numCols}, setGridValues] = useState({
         numRows: 40,
         numCols: 60
     }) 
-
+    
     const[grid, setGrid] = useState(()=>{
         return generateEmptyGrid(numRows, numCols)
     })
+    
+    const [running, setRunning] = useState(false);
+
+    const runningRef = useRef(); //useRef hook update to current value of running state
+    runningRef.current = running
+
+    const genRef = useRef();     //useRef hook to update to current value of generations
+    genRef.current = generations; 
+
+    
+    //run the cell automaton 
+    const runSimulation = useCallback(()=>{  //useCallback hook don't have to make function more than once    
+        if (!runningRef.current){
+            return;
+        }
+
+        //seGenerations to iterate over item and increment by count +1
+        setGenerations((genCount)=>{
+            return (genCount = genCount + 1)
+        })
+
+        //update value in the grid and mutate them
+        setGrid((g)=>{
+            return produce(g, gridCopy=>{
+                for(let i = 0; i <numRows; i++){
+                    for (let k = 0; k < numCols; k++){ 
+                        //compute number of neighbors around cell
+                        let neighbors = 0; 
+                        operations.forEach(([x,y])=>{
+                            const newI = i +x;
+                            const newK = k +y;
+                            //checking the bounds of grid
+                            if(newI >= 0  && newI < numRows && newK >=0 && newK < numCols){
+                                neighbors +=g[newI][newK] //add 1 to current live cell
+                            }
+                        })
+                        // grid copy will die according to rules
+                        if(neighbors < 2 || neighbors > 3){
+                            gridCopy[i][k] = 0; 
+                        //grid copy will be alive according to rules 
+                        }else if (g[i][k] === 0 && neighbors === 3){
+                            gridCopy[i][k] = 1; 
+                        }
+                    }
+                }
+            })
+        })
+        setTimeout(runSimulation, 1000) //run again in 100 milliseconds
+    },[])
 
     return(
         <>
--- game-of-life/src/components/junk.js
+++ game-of-life/src/components/junk.js
@@ -1,24 +1,342 @@
+// var Cell = React.createClass({
+//     propTypes: {
+//       status: React.PropTypes.string,
+//       id: React.PropTypes.string,
+//       handleToggleCell: React.PropTypes.func
+//     },
+  
+//     toggleCell: function (event) {
+//       $(event.target).toggleClass('alive').removeClass('old');
+//       this.props.handleToggleCell(event.target.id);
+//     },
+  
+//     render: function render() {
+//       return (
+//         <div className={"cell " + this.props.status} id={this.props.id}
+//              onClick={this.toggleCell}></div>
+//       );
+//     }
+//   });
+  
+//   var ControlButtons = React.createClass({
+//     propTypes: {
+//       running: React.PropTypes.bool,
+//       handleStart: React.PropTypes.func,
+//       handleClear: React.PropTypes.func,
+//       handlePause: React.PropTypes.func
+//     },
+  
+//     handleClick: function (event) {
+//       $('button.active').removeClass('active');
+//       if (event.target !== this.clearButton) {
+//         $(event.target).addClass('active');
+//       }
+//       if (event.target === this.startButton) {
+//         this.props.handleStart();
+//       } else if (event.target === this.pauseButton) {
+//         this.props.handlePause();
+//       } else if (event.target === this.clearButton) {
+//         this.props.handleClear();
+//       }
+//     },
+  
+//     /* eslint-disable no-return-assign */
+//     render: function render() {
+//       return (
+//         <div className="btn-group btn-group-xs text-left" role="group" aria-label="start | pause | clear ">
+//           <button type="button" className="btn btn-default active"
+//                   ref={(strt) => this.startButton = strt} onClick={this.handleClick}>Start
+//           </button>
+//           <button type="button" className="btn btn-default"
+//                   ref={(pse) => this.pauseButton = pse} onClick={this.handleClick}>Pause
+//           </button>
+//           <button type="button" className="btn btn-default"
+//                   ref={(cls) => this.clearButton = cls} onClick={this.handleClick}>Clear
+//           </button>
+//         </div>
+//       );
+//     }
+//   });
+  
+//   var GenCounter = React.createClass({
+//     propTypes: {
+//       genCount: React.PropTypes.number
+//     },
+  
+//     render: function render() {
+//       return (
+//         <p className="text-right generations">Generations <span className="counter">{this.props.genCount}</span></p>
+//       );
+//     }
+//   });
+  
+  
+//   var GameOfLife = React.createClass({
+//     getInitialState: () => {
+//       return {
+//         gridSize: {x: 45, y: 30},
+//         generations: 0,
+//         running: true
+//       };
+//     },
+  
+//     generateGrid: function () {
+//       let state = this.state;
+//       let y = state.gridSize.y;
+//       let x = state.gridSize.x;
+  
+//       for (let i = 0; i < y; i++) {
+//         for (let j = 0; j < x; j++) {
+//           let key = 'y=' + i + 'x=' + j;
+//           let status = this.generateInitialStatus();
+//           state[key] = {id: key, status: status, x: j, y: i};
+//         }
+//       }
+  
+//       for (let i = 0; i < y; i++) {
+//         for (let j = 0; j < x; j++) {
+//           let cell = state['y=' + i + 'x=' + j];
+//           cell.neighbors = this.getCellNeighbors(cell);
+//         }
+//       }
+  
+//       state.generations = state.generations + 1;
+//       this.setState(state);
+//     },
+  
+//     calculateStatus: function () {
+//       this.time = new Date().getTime();
+  
+//       const currentState = this.state;
+//       const newState = this.cloneState(currentState);
+//       for (let row = 0; row < currentState.gridSize.y; row++) {
+//         for (let col = 0; col < currentState.gridSize.x; col++) {
+//           let cell = currentState['y=' + row + 'x=' + col];
+//           let aliveNeighbors = this.getAliveNeighborCount(cell.neighbors);
+//           let alive = _.includes(cell.status, 'alive');
+//           let old = _.includes(cell.status, 'old');
+  
+//           if (alive && aliveNeighbors < 2) {
+//             newState[cell.id].status = [];
+//           } else if (alive && (aliveNeighbors >= 2 && aliveNeighbors <= 3) && !old) {
+//             newState[cell.id].status.push('old');
+//           } else if (alive && aliveNeighbors > 3) {
+//             newState[cell.id].status = [];
+//           } else if (!alive && aliveNeighbors === 3) {
+//             newState[cell.id].status.push('alive');
+//           }
+//         }
+//       }
+  
+//       newState.generations = currentState.generations + 1;
+//       this.setState(newState);
+//       let now = new Date().getTime();
+//       let delta = now - this.time;
+  
+//       while (delta < 100) {
+//         now = new Date().getTime();
+//         delta = now - this.time;
+//       }
+//       this.intervalID = requestAnimationFrame(this.calculateStatus);
+//     },
+  
+//     getAliveNeighborCount: function (neighbors) {
+//       let aliveNeighbors = 0;
+//       for (let n = 0; n < neighbors.length; n++) {
+//         let neighbor = this.state[neighbors[n]];
+//         if (neighbor && _.includes(neighbor.status, 'alive')) {
+//           aliveNeighbors = aliveNeighbors + 1;
+//         }
+//       }
+//       return aliveNeighbors;
+//     },
+  
+//     cloneState: function (state) {
+//       return _.cloneDeep(state);
+//     },
+  
+//     getCellNeighbors: function (cell) {
+//       const state = this.state;
+//       const x = cell.x;
+//       const y = cell.y;
+//       const gx = state.gridSize.x;
+//       const gy = state.gridSize.y;
+//       let neighbors = [];
+  
+//       // Left Edge
+//       if (x === 0) {
+//         neighbors.push('y=' + (y - 1) + 'x=' + (gx - 1));
+//         neighbors.push('y=' + (y) + 'x=' + (gx - 1));
+//         neighbors.push('y=' + (y + 1) + 'x=' + (gx - 1));
+//       }
+  
+//       // Top Left Corner
+//       if (x === 0 && y === 0) {
+//         neighbors.push('y=' + (gy - 1) + 'x=' + (gx - 1));
+//       }
+  
+//       // Top Edge
+//       if (y === 0) {
+//         neighbors.push('y=' + (gy - 1) + 'x=' + (x - 1));
+//         neighbors.push('y=' + (gy - 1) + 'x=' + (x));
+//         neighbors.push('y=' + (gy - 1) + 'x=' + (x + 1));
+//       }
+  
+//       // Right Edge
+//       if (x === gx - 1) {
+//         neighbors.push('y=' + (y - 1) + 'x=' + (0));
+//         neighbors.push('y=' + (y) + 'x=' + (0));
+//         neighbors.push('y=' + (y + 1) + 'x=' + (0));
+//       }
+  
+//       // Bottom Edge
+//       if (y === gy - 1) {
+//         neighbors.push('y=' + (0) + 'x=' + (x - 1));
+//         neighbors.push('y=' + (0) + 'x=' + (x));
+//         neighbors.push('y=' + (0) + 'x=' + (x + 1));
+//       }
+  
+//       // Top Right Corner
+//       if (x === gx - 1 && y === 0) {
+//         neighbors.push('y=' + (gy - 1) + 'x=' + (0));
+//       }
+  
+//       // Bottom Right Corner
+//       if (x === gx - 1 && y === gy - 1) {
+//         neighbors.push('y=' + (0) + 'x=' + (0));
+//       }
+  
+//       // Bottom Left Corner
+//       if (x === 0 && y === gy - 1) {
+//         neighbors.push('y=' + (0) + 'x=' + (gx - 1));
+//       }
+  
+//       // Everything in between
+//       neighbors.push('y=' + (y - 1) + 'x=' + (x - 1));
+//       neighbors.push('y=' + (y - 1) + 'x=' + (x));
+//       neighbors.push('y=' + (y - 1) + 'x=' + (x + 1));
+//       neighbors.push('y=' + (y) + 'x=' + (x - 1));
+//       neighbors.push('y=' + (y) + 'x=' + (x + 1));
+//       neighbors.push('y=' + (y + 1) + 'x=' + (x - 1));
+//       neighbors.push('y=' + (y + 1) + 'x=' + (x));
+//       neighbors.push('y=' + (y + 1) + 'x=' + (x + 1));
+//       return neighbors;
+//     },
+  
+//     generateInitialStatus: function () {
+//       let num = Math.floor((Math.random() * 3));
+//       switch (num) {
+//         case 0:
+//           return [];
+//         case 1:
+//           return ['alive'];
+//         case 2:
+//           return ['alive', 'old'];
+//         default:
+//           return [];
+//       }
+//     },
+  
+//     componentWillMount: function () {
+//       this.generateGrid();
+//     },
+  
+//     componentDidMount: function () {
+//       this.intervalID = requestAnimationFrame(this.calculateStatus);
+//     },
+  
+//     componentWillUnmount: function () {
+//       cancelAnimationFrame(this.intervalID);
+//     },
+  
+//     handlePause: function () {
+//       if (this.state.running) {
+//         cancelAnimationFrame(this.intervalID);
+//         this.setState({running: false});
+//       }
+//     },
+  
+//     handleStart: function () {
+//       if (!this.state.running) {
+//         this.intervalID = requestAnimationFrame(this.calculateStatus);
+//         this.setState({running: true});
+//       }
+//     },
+  
+//     handleClear: function () {
+//       var state = this.state;
+//       const x = this.state.gridSize.x;
+//       const y = this.state.gridSize.y;
+  
+//       cancelAnimationFrame(this.intervalID);
+  
+//       for (let i = 0; i < y; i++) {
+//         for (let j = 0; j < x; j++) {
+//           let key = 'y=' + i + 'x=' + j;
+//           state[key].status = [];
+//         }
+//       }
+//       state.generations = 0;
+//       state.running = false;
+//       this.setState(state);
+//     },
+  
+//     handleToggleCell: function (id) {
+//       const cell = this.state[id];
+//       if (_.includes(cell.status, 'alive')) {
+//         cell.status = [];
+//       } else {
+//         cell.status = ['alive'];
+//       }
+//       this.setState(cell);
+//     },
+  
+//     render: function render() {
+//       let cells = [];
+//       for (let i = 0; i < this.state.gridSize.y; i++) {
+//         for (let j = 0; j < this.state.gridSize.x; j++) {
+//           let cell = this.state['y=' + i + 'x=' + j];
+//           cells.push(<Cell key={cell.id} id={cell.id} handleToggleCell={this.handleToggleCell}
+//                            status={cell.status.join(' ')}/>);
+//         }
+//       }
+  
+//       return (
+//         <div className="container-fluid">
+//           <div className="row">
+//             <h1 className="text-primary text-center">Game of Life</h1>
+//           </div>
+//           <div className="row col-lg-6 col-lg-offset-3 col-md-offset-2 col-sm-offset-1">
+//             <div className="col-lg-6">
+//               <ControlButtons handleStart={this.handleStart} handlePause={this.handlePause}
+//                               running={this.state.running} handleClear={this.handleClear}/>
+//             </div>
+//             <div className="col-lg-6">
+//               <GenCounter genCount={this.state.generations}/>
+//             </div>
+//           </div>
+//           <div className="row col-lg-6 col-lg-offset-3 col-md-offset-2 col-sm-offset-1">
+//             <div id="board">
+//               {cells}
+//             </div>
+//           </div>
+//           <div className="row col-lg-6 col-lg-offset-3 col-md-offset-2 col-sm-offset-1 text-center">
+//             <p id="footerText">More information about {" "}
+//               <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">Conway's Game of Life</a>.</p>
+//           </div>
+//         </div>
+//       );
+//     }
+//   });
+  
+//   ReactDOM.render(
+//     <GameOfLife />,
+//     document.getElementById('app')
+//   );
 
-//set up initial state with useState hook
-// const [generations, setGenerations] = useState(0)
-// const [speed, setSpeed] = useState(100)
-// const [numRows, setNumRow] = useState(40)
-// const [numColumns, setNumColumns] = useState(60)
-// const grid =useState([])
-// const [grid, setGrid] = useState(Array(numRows).fill().map(()=>Array(numColumns).fill(false))) //generates empty grid
 
 
-//changes grid when box is selected
-// const selectBox =(row, column)=>{
-//     let gridCopy = JSON.parse(JSON.stringify(grid)) //creates copy of grid. Double Buffering
-//     gridCopy[row][column] =!gridCopy[row][column] //toggle between alive and dead cell state
-
-//     // gridCopy[row][columns] =gridCopy[row][column] > 0? 0: 1;
-//      //if number of row and columns greater than 0, set default to 0. dead. else set to alive 
-
-//      //change new setGrid state to new gridCopy from box selected
-//     setGrid(gridCopy)
-// }
 
 
-// const [grid, setGrid] = useState(Array(numRows).fill().map(()=>Array(numColumns).fill(false))) //generates empty grid
+
+
